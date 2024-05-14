4363d7fc46229464ae8ac0d696860f17bb19bcc9
Sudarshen <sudarshenk@gmail.com>
2024-05-14 10:35:06 +0200

    yow yow yow 1 excercise 6 commit

---
+++ Exercise 6.py
@@ -0,0 +1,27 @@
+# # Exercise 1
+# user_list1 = []
+#
+# while True:
+#     user_input = input("Enter a element to make the first list \n"
+#                        "(or press Enter to finish the first list): ")
+#     if user_input == "":
+#         break
+#     user_list1.append(user_input)
+# user_list2 = []
+# while True:
+#     user_input2 = input("Enter a element to make the second list \n "
+#                         "(or press Enter to finish the second list): ")
+#     if user_input2 == "":
+#         break
+#     user_list2.append(user_input2)
+#
+# # # key
+# # # user_list1 = [1, 2, 3, 4]
+# # # value
+# # # user_input2 = [7, 8, 9, 10]
+# # # enjoin
+#
+# my_dict = dict(zip(user_list1, user_list2))
+# print(my_dict)
+
+# Exercise 2
